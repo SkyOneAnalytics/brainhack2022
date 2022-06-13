1c34f0433fea66edb8269ae859df2132c2f43d97
timchang27 <timothychang27@gmail.com>
2022-06-13 22:37:59 +0800

    Training Screen changes

--- screens/TrainingScreen.tsx
+++ screens/TrainingScreen.tsx
@@ -1,13 +1,35 @@
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { ImageBackground, ScrollView } from 'react-native';
+import { RootTabScreenProps } from '../types';
 import styles from '../styles'
+import { ReactChild, ReactFragment, ReactPortal } from 'react';
+import CountdownTimer from '../components/CountdownTimer';
+
+const ItemList = (props: { name: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; }) => {
+  return (
+    <View style={styles.itemList}>
+      <Text>{props.name}</Text>
+    </View>
+  );
+}
 
 export default function TrainingScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Today's Activities</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TrainingScreen.tsx" />
+      <View style={styles.programmeBox}>
+        <View style={styles.listItem}>
+          <ItemList name='5BX' />
+          <ItemList name='Army Recruitment Talk' />
+          <ItemList name='Mealtime Regime' />
+          <ItemList name='Lunch' />
+          <ItemList name='Admin Time' />
+          <ItemList name='24KM Route March' />
+          <ItemList name='Wash Up' />
+          <ItemList name='Lights Out' />
+        </View>
+      </View>
     </View>
   );
 }
